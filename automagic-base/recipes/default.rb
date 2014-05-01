634a4e86794685834ebeec85542a551c227fde59
Nathan Lee <nathan@globalphobia.com>
2014-04-30 18:43:50 -0700

    cleanup

--- automagic-base/recipes/default.rb
+++ automagic-base/recipes/default.rb
@@ -1,12 +1,3 @@
-#
-# Cookbook Name:: automagic-base
-# Recipe:: default
-#
-# Copyright (C) 2014 YOUR_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
 include_recipe 'apt'
 include_recipe 'git'
 include_recipe 'build-essential'
